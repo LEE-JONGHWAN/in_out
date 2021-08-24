16c13faa9b038c2559d0fb71fc1d539de077869f
Lee JongHwan <jong479@gmail.com>
2021-08-24 17:58:46 +0900

    updated 8/24
    First input output studying

---
+++ src/sec02/exam01_inputstream_read/ReadExample1.java
@@ -0,0 +1,22 @@
+﻿package sec02.exam01_inputstream_read;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class ReadExample1 {
+
+	public static void main(String[] args) throws Exception {
+		InputStream is = new FileInputStream("C:/Temp/test.txt");
+		int readByte;
+		int readCount = 0;
+		while(true) {
+			readByte = is.read();
+			readCount++;
+			if(readByte == -1) break;
+			System.out.println((char)readByte);
+		}
+		is.close();
+		System.out.println("읽은 바이트 수 : " + readCount);
+	}
+
+}
---
+++ src/sec02/exam01_inputstream_read/ReadExample2.java
@@ -0,0 +1,22 @@
+﻿package sec02.exam01_inputstream_read;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class ReadExample2 {
+
+	public static void main(String[] args) throws Exception {
+		InputStream is = new FileInputStream("C:/Temp/test.txt");
+		int readByteNo;
+		byte[] readBytes = new byte[3];
+		var data = new StringBuilder();
+		while( true ) {
+			readByteNo = is.read(readBytes);
+			if(readByteNo == -1) break;
+			data.append(new String(readBytes, 0, readByteNo));
+		}
+		System.out.println(data);
+		is.close();
+	}
+
+}
